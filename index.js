b7f63f7e6575b37eeaaf8f5395913d2e6caefd72
Partizer <159246364+Partizer@users.noreply.github.com>
2024-02-07 10:35:22 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+function vis1(id) {
+    var e = document.getElementById(id);
+    if(e.style.display == 'none')
+       e.style.display = 'block';
+    else
+       e.style.display = 'none';}
+       
+function vis2(id) {
+    var e = document.getElementById(id);
+    if(e.style.display == 'none')
+       e.style.display = 'block';
+    else
+       e.style.display = 'none';}
+
+function vis3(id) {
+    var e = document.getElementById(id);
+    if(e.style.display == 'none')
+       e.style.display = 'block';
+    else
+       e.style.display = 'none';}
+           
+function vis4(id) {
+    var e = document.getElementById(id);
+    if(e.style.display == 'none')
+       e.style.display = 'block';
+    else
+       e.style.display = 'none';}
+               
+function vis5(id) {
+    var e = document.getElementById(id);
+    if(e.style.display == 'none')
+       e.style.display = 'block';
+    else
+       e.style.display = 'none';}
+                        
+    
